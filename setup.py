80230affd055bab7671e3edde8c09e9c157390a0
Antti Kaihola <akaihol+github@ambitone.com>
2011-01-21 21:58:54 +0200

    Added translation files

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     entry_points = {
         'console_scripts': ['manage = ylaneenkasvit.manage:main'],
     },
+    include_package_data = True,
     install_requires = ['django', 'gunicorn'],
 )
