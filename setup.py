dc7d49ceafb96051273e520211b834e0892102fb
Antti Kaihola <akaihol+github@ambitone.com>
2011-01-21 13:38:19 +0200

    Added console script entry point for manage.py

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setup(
     name = "ylaneenkasvit",
     version = "0.1",
     packages = find_packages(),
+    entry_points = {
+        'console_scripts': ['manage = ylaneenkasvit.manage:main'],
+    },
 )
