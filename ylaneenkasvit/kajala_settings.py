de3d8523e647e9226cae17ff7ab4b55fc1e8558e
Antti Kaihola <akaihol+github@ambitone.com>
2011-09-17 15:35:16 +0300

    Added site-specific settings for kajalankasvit

---
+++ ylaneenkasvit/kajala_settings.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+from .common_settings import *
+
+SITE_ROOT = '/www/kajalankasvit'
+
+ADMINS = (('Antti Kaihola', 'akaihol+kajalankasvit@ambitone.com'),)
+MANAGERS = ADMINS
+
+DATABASES['default'].update({
+    'NAME': 'kajalankasvit',
+    'USER': 'kajalankasvit',
+    'PASSWORD': '6dofoso11'})
+
+STATIC_URL = 'http://static.kajalankasvit.ambitone.com/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'grappelli/'
+
+GRAPPELLI_ADMIN_TITLE = u'Kajalan kasvimaat'
+MEDIA_URL = 'http://media.kajalankasvit.ambitone.com'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
+
+try:
+    from ylaneenkasvit.local_settings import *
+    modify(globals())
+except ImportError:
+    pass
