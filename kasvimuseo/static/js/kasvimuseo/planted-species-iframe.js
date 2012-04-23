c3ddf48386d06123f3cc6c22515219d92e0c8776
Antti Kaihola <akaihol+github@ambitone.com>
2012-04-23 13:31:00 +0300

    IE compatibility for iframe size trick

--- kasvimuseo/static/js/kasvimuseo/planted-species-iframe.js
+++ kasvimuseo/static/js/kasvimuseo/planted-species-iframe.js
@@ -1,18 +1,16 @@
 document.write('<iframe src="http://kasvit.ambitone.com/kasvimuseo/planted-species/" width="547px" height="10000px" scrolling="no" id="kasvit-iframe" style="border: none;"></iframe>');
 
 (function() { 
-    function getTop(elem) {
-        var y = elem.offsetTop;
-        while (elem = elem.offsetParent) y += elem.offsetTop;
-        return y;
-    }
-
-    window.addEventListener(
-        "message",
-        function(e) {
-            var iframe = document.getElementById("kasvit-iframe");
-            iframe.style.height = e.data.plantedSpeciesIframeHeight;
-            window.scrollTo(0, getTop(iframe));
+    var getTop = function(elem) {
         },
-        false);
+        resizeIframe = function(event) {
+            var elem = document.getElementById("kasvit-iframe"),
+                y = elem.offsetTop;
+            elem.style.height = event.data.plantedSpeciesIframeHeight;
+            while (elem = elem.offsetParent) y += elem.offsetTop;
+            window.scrollTo(0, y);
+        };
+    
+    if (window.addEventListener) window.addEventListener("message", resizeIframe, false);
+    else window.attachEvent("message", resizeIframe);
 })();
