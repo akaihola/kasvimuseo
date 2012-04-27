f828f29fbe53566d5938100196135cdcb4ac569b
Antti Kaihola <akaihol+github@ambitone.com>
2012-04-27 21:34:25 +0300

    Fix iframe height after 500 ms if the content has grown

--- kasvimuseo/static/js/kasvimuseo/iframe-height-fix.js
+++ kasvimuseo/static/js/kasvimuseo/iframe-height-fix.js
@@ -1,7 +1,19 @@
 $(window).on('pagechange', function(event, data) {
-    parent.postMessage(
-        {plantedSpeciesIframeHeight: Math.floor($('.ui-footer', data.toPage).position().top) + 'px',
-         plantedSpeciesIframePlantName: $('.finnish-name', data.toPage).text().trim()},
-        '*'
-    );
+    var getHeight = function() {
+            return Math.floor($('.ui-footer', data.toPage).position().top);
+        },
+        postHeight = function(height) {
+            parent.postMessage(
+                {plantedSpeciesIframeHeight: height + 'px',
+                 plantedSpeciesIframePlantName: $('.finnish-name', data.toPage).text().trim()},
+                '*'
+            );
+        },
+        firstHeight = getHeight();
+    postHeight(firstHeight);
+
+    setTimeout(function() {
+        var newHeight = getHeight();
+        if (newHeight > firstHeight) postHeight(newHeight);
+    }, 500);
 });
