b4b25b62a4edb0e2100740ac7a91a60bf3ddb85f
Antti Kaihola <akaihol+github@ambitone.com>
2013-06-04 11:26:52 +0300

    Added logging settings - gunicorn fails otherwise, maybe this helps?

--- ylaneenkasvit/common_settings.py
+++ ylaneenkasvit/common_settings.py
@@ -76,5 +76,31 @@ SOUTH_MIGRATION_MODULES = {
     'photologue': 'ylaneenkasvit.external_migrations.photologue',
 }
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+
 DEBUG = False
 TEMPLATE_DEBUG = True
