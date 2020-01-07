314ceacfbf02d1e57919bd7e23f0a645849537e9
Antti Kaihola <antti+github@kaihola.fi>
2020-01-07 20:11:08 +0200

    Package templates. Version 0.2.0.

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "ylaneenkasvit",
-    version = "0.1",
-    packages = find_packages(),
-    entry_points = {
+    name="ylaneenkasvit",
+    version="0.2.0",
+    packages=find_packages(),
+    entry_points={
         'console_scripts': ['manage = ylaneenkasvit.manage:main'],
     },
     include_package_data=True,
-    package_data={'kasvimuseo': ['static/**/*', 'static/**/**/*']},
+    package_data={'kasvimuseo': ['static/**/*',
+                                 'static/**/**/*',
+                                 'templates/**/*',
+                                 'templates/**/**/*']},
     install_requires=[line for line in open('requirements/production.txt')],
     tests_require=['mock==2.0.0',
                    'pbr==4.0.2',
